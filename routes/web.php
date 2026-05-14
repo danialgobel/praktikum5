eb6b4582da4ac692902c948ca1d77065ce7f42fd
Praktikum User <praktikum@example.com>
2026-05-14 15:04:02 +0700

    Complete praktikum routing task

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,11 @@ Route::get('/about', function () {
 Route::get('/user/{name}', function ($name) {
     return "Halo, $name!";
 });
+
+Route::get('/product/{id}', function ($id) {
+    return "Produk dengan ID: $id";
+});
+
+Route::get('/city/{name}', function ($name) {
+    return "Kota: $name";
+});
